663a7c08050e1e01041198701664dd91933d1a04
pradeepkotha88 <pradeepkotha88@gmail.com.com>
2017-08-22 23:29:44 -0500

    sorting an array

---
+++ assignment/Pradeep/session12/ascndesc.js
@@ -0,0 +1,10 @@
+//ascending order
+var array=['amit','varun','hari','navya','deep']
+array.sort();
+
+
+//descending order
+
+var array=['amit','varun','hari','navya','deep']
+array.sort();
+array.reverse();
